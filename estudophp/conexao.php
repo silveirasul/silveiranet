6c230cd95ba0790b30372a9901d259dc60f58d1e
silveirasul <silveirasul@gmail.com>
2017-03-31 16:59:03 -0300

    Subindo versão 1.0

---
+++ estudophp/conexao.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+
+try{
+	$conn = new \PDO("mysql:host=localhost;dbname=teste","root","root");
+	
+	
+} catch(\PDOException $e) {
+	echo "Error! Message:".$e -> getMessage(). "code:". $e->getCode();
+	
+}
+
+
+
+
+
+
+
+
+?>
---
+++ estudophp/index.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+class pessoas {
+
+var $nome, $idade, $telefone;
+
+function esc_nome($nom, $ida, $tel) {
+     $this -> nome = $nom;
+	 $this -> idade = $ida;
+	 $this -> telefone = $tel;
+}
+  
+ 
+}
+
+$mulher = new pessoas();
+$mulher  -> esc_nome('Maria Paula', '25 anos', '81368494');
+echo $mulher -> nome. "<br>";
+echo $mulher -> idade. "<br>";
+echo $mulher -> telefone. "<br>";
+
+
+?>
